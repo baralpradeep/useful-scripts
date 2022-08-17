433dda61fca8bc93f185f1a9a2bd57681b536ac7
Pradeep Baral <52475976+baralpradeep@users.noreply.github.com>
2022-08-17 10:33:22 -0400

    Update script_to _rename.sh
    rename a file if it has a name value pair

--- script_to _rename.sh
+++ script_to _rename.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
+rm -rf /Users/projects/new-policy
+mkdir /Users/projects/new-policy
 for i in `find . -maxdepth 1 -type d | sed 's|^./||'`;
 do
-#    cd $i
-    DIR=$i
-   # echo $DIR
+
     for l in `find $i -maxdepth 1 -type d`;
     do
       FILE=$l/azurepolicy.json
       if test -f "$FILE"; then
-         #echo "$FILE exists."
-         #echo "file Name" $FILE
-         FILE2=`cat $FILE | jq -r ".[].displayName" |sed 's/ /-/g'`.json
-         #FILE2=`echo $l |sed 's/\//-/g'`.json
-         echo $FILE2
-         mv $FILE  /Users/pradeep.baral/projects/new-policy/$FILE2
+         FILE2=`cat $FILE | jq -r ".[].displayName" |sed 's/ /_/g'`
+         if [ -z "$FILE2" ]
+          then
+            echo $FILE2  "is empty"
+          else
+            NEW=`echo $i"_"$FILE2`.json
+            mv $FILE  /Users/projects/new-policy/$NEW
+         fi
 
       fi
    done
